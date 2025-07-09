affdac854974c560709045bde4363c63ce3da887
cahya kumala <cahyakumala573@gmail.com>
2025-07-09 17:41:55 +0800

    fix some code

--- practice/src/FullMoons.java
+++ practice/src/FullMoons.java
@@ -7,9 +7,9 @@ class FullMoons {
         c.set(2004,0,7,15,40);
         long day1 = c.getTimeInMillis();
         for (int x = 0; x < 60; x++) {
-            day1 += (DAY_IM * 29.52);
+            day1 += (long) (DAY_IM * 29.52);
             c.setTimeInMillis(day1);
-            out.println(String.format("full moon on %tc", c));
+            out.printf("full moon on %tc%n", c);
         }
     }
 }
